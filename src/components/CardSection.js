9638d891934d8b021463b55f66b9fc4677f4720e
Nguyen Louis <nguyenlouis172@gmail.com>
2024-06-27 04:41:10 +0200

    add component section card

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -1,6 +1,25 @@
 import styles from "../styles/CardSection.module.css";
+import Card from "./Card";
 
-function CardSection() {
-  return <div></div>;
+function CardSection(props) {
+  return (
+    <section className={styles.sectionContainer}>
+      {/* Loop through the sectionData arra*/}
+      {props.sectionData.map((card) => {
+        {
+          /* Render a  Card component for each object*/
+        }
+        return (
+          <Card
+            key={card.id}
+            image={card.image}
+            title={card.title}
+            address={card.category}
+            price={card.price}
+          />
+        );
+      })}
+    </section>
+  );
 }
 export default CardSection;
