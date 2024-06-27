1efa7be6f3665fc6ad57094b6d7e5a1e754fdeb2
Nguyen Louis <nguyenlouis172@gmail.com>
2024-06-27 04:26:39 +0200

    add component card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,17 @@
 import styles from "../styles/Card.module.css";
 
-function Card() {
-  return <div></div>;
+function Card(props) {
+  return (
+    <div className={styles.cardContainer}>
+      <img className={styles.imageCard} src={props.image} />
+      <h2 className={styles.titleCard}>{props.title}</h2>
+      <div>
+        <p className={styles.addressCard}>{props.address}</p>
+      </div>
+      <div>
+        <p className={styles.priceCard}>{props.price}</p>
+      </div>
+    </div>
+  );
 }
 export default Card;
